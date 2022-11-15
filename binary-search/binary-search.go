4c5b7e623c986cd53fcde556438c9acbe58ad7ce
Sourabh Jha <sourabhjha96444@gmail.com>
2022-11-15 12:54:16 +0530

    feat: added main function binary-search

--- binary-search/binary-search.go
+++ binary-search/binary-search.go
@@ -1,21 +1,21 @@
-package binarysearch
+package main
 
 import (
+	"fmt"
 	"sort"
 )
 
-func IterativebinarySearch(arr []int, target int) int {
+func binarySearch(arr []int, x int) int {
 	sort.Ints(arr)
-	low := 0
+	low := arr[0]
 	high := len(arr)
-
 	for low <= high {
-		mid := low + (high-low)/2
-
-		if mid == target {
+		mid := (high + low) / 2
+		// fmt.Printf("Mid is %v and x is %v", mid, x)
+		if mid == x {
 			return mid
 		}
-		if arr[mid] < target {
+		if arr[mid] < x {
 			low = mid + 1
 		} else {
 			high = mid - 1
@@ -24,17 +24,21 @@ func IterativebinarySearch(arr []int, target int) int {
 	return -1
 }
 
-func RecursiveBinarySearch(arr []int, low int, high int, target int) int {
-	if high >= low {
-		mid := low + (high-low)/2
+func main() {
+	fmt.Println("This program is meant to perform the binary search algorithm on your input.")
+	var size int
 
-		if arr[mid] == target {
-			return mid
-		}
-		if arr[mid] > target {
-			return RecursiveBinarySearch(arr, low, mid-1, target)
-		}
-		return RecursiveBinarySearch(arr, mid+1, high, target)
+	fmt.Printf("Enter the size of the array :")
+	// fmt.Scanf(size)
+	fmt.Scanln(&size)
+	var arr = make([]int, size)
+	for i := 0; i < size; i++ {
+		fmt.Printf("Enter the %dth element\n", i)
+		fmt.Scan(&arr[i])
 	}
-	return -1
+	var x int
+	fmt.Println("Enter the key you want to search: ")
+	fmt.Scan(&x)
+	fmt.Println(binarySearch(arr, x))
+
 }
